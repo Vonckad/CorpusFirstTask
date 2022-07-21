65d66e51b29643edff5556518b942f76adff3a80
Vonckad <Vonkad@yandex.ru>
2022-07-21 11:38:01 +0300

    add googleMap, Alamofire and show map

--- CorpusFirstTask/ViewController.swift
+++ CorpusFirstTask/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    
+    var googleMapView = GMSMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        let initialLocation = CLLocation(latitude: 53.9, longitude: 27.5667)
+        googleMapView = .map(withFrame: view.frame, camera: GMSCameraPosition(target: initialLocation.coordinate, zoom: 14))
+        googleMapView.delegate = self
+        googleMapView.isMyLocationEnabled = true
+        view.addSubview(googleMapView)
     }
-
-
 }
 
+extension ViewController: GMSMapViewDelegate {
+    
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CorpusFirstTask' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CorpusFirstTask
+	
+	pod 'GoogleMaps'
+	pod 'Alamofire'
+
+  target 'CorpusFirstTaskTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CorpusFirstTaskUITests' do
+    # Pods for testing
+  end
+
+end
